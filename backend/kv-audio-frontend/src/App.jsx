eb9c519e40475db1b0db23e2fd1b55397b618a8b
Sakindu Pabasara <sakindup2001@gmail.com>
2025-07-11 11:58:24 +0530

    init👌

---
+++ backend/kv-audio-frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+import ProductCard from "./components/productCard";
+
+function App() {
+  return (
+    <div>
+      <ProductCard name="Audio setup" price="3250/=" 
+      description="dfvsf fscsdc sdcsdc sddswdc" 
+      photoUrl="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb5EWPwRAqt5sl4qk8LuMjK9zdRAg4YWdBVw&s" />
+
+      <ProductCard name="Phone case" price="1500/=" 
+      description="AnDyH Phone case For Xiaomi POCO M3 Pro 5G Case,Creative Fashion Luxury New 3D Swan Retractable Stand Phone Case Premium Simple Solid Color Straight Edge Plating Soft Silicone Shockproof Casing Protective Back Cover"
+      photoUrl="https://img.drz.lazcdn.com/static/lk/p/481b8187fae456bf439c15fa919bc21f.jpg_720x720q80.jpg_.webp" />
+
+    </div>
+  );
+}
+
+export default App;
--- routes/productRouter.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { addProduct } from "../controllers/productController.js";
-
-const productRouter = express.Router();
-
-productRouter.post("/",addProduct)
-
-export default productRouter;
