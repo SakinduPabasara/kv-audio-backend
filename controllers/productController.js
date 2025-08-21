6d7ea1266a303eb18d0ff96daec1b0c12479cfce
Sakindu Pabasara <sakindup2001@gmail.com>
2025-08-22 02:16:30 +0530

    add a return statement👍

--- controllers/productController.js
+++ controllers/productController.js
@@ -2,104 +2,105 @@ import Product from "../models/product.js";
 import { isItAdmin } from "./userController.js";
 
 // Add a new product
-export async function addProduct(req,res){
+export async function addProduct(req, res) {
 
-    if(req.user == null){
+    if (req.user == null) {
         res.status(401).json({
-            message : "Please login and try again"
+            message: "Please login and try again"
         })
         return    // මෙතෙනදී return කරන්නෙ, function එකෙන් ඉවත් වෙන්න. එහෙම නැති උනොත්, code එක ඉදිරියට execute වෙනවා දිගටම.
     }
-    if(req.user.role !="admin"){
+    if (req.user.role != "admin") {
         res.status(403).json({
-            message : "You are not authorized to  perform this action"
+            message: "You are not authorized to perform this action"
         })
         return
     }
 
     const data = req.body;
     const newProduct = new Product(data);
-    try{
+    try {
         await newProduct.save();
         res.json({
-            message : "Product registered successfully"
+            message: "Product registered successfully"
         })
-    }catch(error){
+    } catch (error) {
         res.status(500).json({
-            error : "Product rgistration failed"
+            error: "Product registration failed"
         })
     }
 }
 
 // Get all products or available products
-export async function getProducts(req,res){
-    
-    try{
+export async function getProducts(req, res) {
 
-        if(isItAdmin(req)){
+    try {
+
+        if (isItAdmin(req)) {
             const products = await Product.find();
             res.json(products);
             return;
-        }else{
-            const products = await Product.find({availability : true});
+        } else {
+            const products = await Product.find({ availability: true });
             res.json(products);
             return;
         }
-        
-    }catch(e){
+
+    } catch (e) {
         res.status(500).json({
-            message : "Failed to get products"
+            message: "Failed to get products"
         })
     }
 }
 
 // Update a product
-export async function updateProduct(req,res){
-    try{
-        if(isItAdmin(req)){
+export async function updateProduct(req, res) {
+    try {
+        if (isItAdmin(req)) {
 
             const key = req.params.key;
 
             const data = req.body;
 
-            await Product.updateOne({key:key},data)
+            await Product.updateOne({ key: key }, data)
 
             res.json({
-                message : "Product updated successfully"
+                message: "Product updated successfully"
             })
             return;
 
-        }else{
+        } else {
             res.status(403).json({
-                message : "You are not authorized to perform this action"
+                message: "You are not authorized to perform this action"
             })
             return;
         }
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
-            message : "Failed to update products"
+            message: "Failed to update products"
         })
     }
 }
 
 // Delete a product
-export async function deleteProduct(req,res){
-    try{
-        if(isItAdmin(req)){
+export async function deleteProduct(req, res) {
+    try {
+        if (isItAdmin(req)) {
             const key = req.params.key;
-            await Product.deleteOne({key:key})
+            await Product.deleteOne({ key: key })
             res.json({
-                message : "Product deleted successfully"
+                message: "Product deleted successfully"
             })
-        }else{
+            return;
+        } else {
             res.status(403).json({
-                message : "You are not authorized to perform this action"
+                message: "You are not authorized to perform this action"
             })
             return;
         }
-    }catch(e){
+    } catch (e) {
         res.status(500).json({
-            message : "Failed to delete product"
+            message: "Failed to delete product"
         })
     }
 }
